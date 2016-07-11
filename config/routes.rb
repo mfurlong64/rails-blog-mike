e04cdef4c8383eb1b294b6f9fae746d7f00eb222
Michael Furlong <furthing45@Michaels-MacBook-Pro-4.local>
2016-07-10 20:40:01 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+ 
+  resources :articles
+ 
+  root 'welcome#index'
+end
+
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
