4db88324dd1e507a67728c90995c6e9daf78a3f1
Michael Furlong <furthing45@Michaels-MacBook-Pro-4.local>
2016-07-10 21:14:33 -0700

    continue Rails tutorial and start post routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-
   def new
+  end
+
+  def create
+  	render plain: params[:article].inspect
   end
 
 end
